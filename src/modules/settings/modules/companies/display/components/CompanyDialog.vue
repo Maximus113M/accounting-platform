1865b3288f5024449bfef87a4b75bf91ff39c2d7
Maximus113M <camilomantillaq13@gmail.com>
2024-09-19 09:05:37 -0500

    FEAT(Company): Company form in process

--- src/modules/settings/modules/companies/display/components/CompanyDialog.vue
+++ src/modules/settings/modules/companies/display/components/CompanyDialog.vue
@@ -51,8 +51,10 @@
                         </div>
                         <div class="col-12 col-sm-6 col-md-4">
                             <div class="q-pb-xs text-subtitle text-weight-medium">Tipo Documento</div>
-                            <q-input outlined dense type="text" v-model="currentCompany.basicData.documentType"
-                                :rules="[(val: string) => (val && val.length > 0) || 'Debes completar este campo']" />
+                            <q-select outlined dense v-model="currentCompany.basicData.documentType"
+                                :options="documentTypes"
+                                :rules="[(val: string) => (val && val.length > 0) || 'Debes completar este campo']">
+                            </q-select>
                         </div>
                         <div class="col-12 col-sm-6 col-md-4">
                             <div class="q-pb-xs text-subtitle text-weight-medium">Documento</div>
@@ -63,6 +65,27 @@
                         <div class="col-12 col-sm-6 col-md-4">
                             <div class="q-pb-xs text-subtitle text-weight-medium">Nombre Comercial</div>
                             <q-input outlined dense type="text" v-model="currentCompany.basicData.companyName"
+                                :rules="[(val: string) => (val && val.length > 0) || 'Debes completar este campo']" />
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-4">
+                            <div class="q-pb-xs text-subtitle text-weight-medium">Ciudad</div>
+                            <q-select outlined dense v-model="currentCompany.basicData.city.name" :options="cities"
+                                :rules="[(val: string) => (val && val.length > 0) || 'Debes completar este campo']" />
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-4">
+                            <div class="q-pb-xs text-subtitle text-weight-medium">Dirección</div>
+                            <q-input outlined dense type="text" v-model="currentCompany.basicData.address"
+                                :rules="[(val: string) => (val && val.length > 0) || 'Debes completar este campo']" />
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-4">
+                            <div class="q-pb-xs text-subtitle text-weight-medium">Dirección</div>
+                            <q-input outlined dense type="text" v-model="currentCompany.basicData.address"
+                                :rules="[(val: string) => (val && val.length > 0) || 'Debes completar este campo']" />
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-4">
+                            <div class="q-pb-xs text-subtitle text-weight-medium">Tipo de Regimen</div>
+                            <q-select outlined dense type="text" v-model="currentCompany.regimeType"
+                                :options="regimeTypeOptions"
                                 :rules="[(val: string) => (val && val.length > 0) || 'Debes completar este campo']" />
                         </div>
                         <div class="col-12 col-sm-6 col-md-4">
@@ -92,18 +115,101 @@
                                 <q-radio v-model="currentCompany.basicData.businessTypeName" label="No" :val="false" />
                             </div>
                         </div>
+                        <div class="col-12 col-sm-6 col-md-4">
+                            <div class="q-pb-xs text-subtitle text-weight-medium">Cobrador por defecto</div>
+                            <q-select outlined dense type="text" v-model="currentCompany.regimeType"
+                                :options="['Camilo', 'Daniel', 'Freddy']"
+                                :rules="[(val: string) => (val && val.length > 0) || 'Debes completar este campo']" />
+                        </div>
 
                     </div>
 
-                    <div v-else-if="selectedTab === 1">
-                        <div class="col-12 text-h6 text-bold">
+                    <div v-else-if="selectedTab === 1" class="row  q-col-gutter-x-xl">
+                        <div class="col-12 text-h6 text-bold q-mb-sm">
                             Tributario
+                        </div>
+
+                        <div class="col-12 col-sm-6 col-md-4">
+                            <div class="q-pb-xs text-subtitle text-weight-medium">Código actividad económica</div>
+                            <q-select outlined dense v-model="currentCompany.taxData.economicActivity"
+                                :options="economicActivities"
+                                :rules="[(val: string) => (val && val.length > 0) || 'Debes completar este campo']">
+                            </q-select>
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-4">
+                            <div class="q-pb-xs text-subtitle text-weight-medium">Tarifa ICA</div>
+                            <q-input outlined dense type="number" v-model.number="currentCompany.taxData.icaRate"
+                                :rules="[(val: number) => (val && val >= 0) || 'Debes completar este campo']" />
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-4">
+                            <div class="q-pb-xs text-subtitle text-weight-medium">Responsabilidades fiscales</div>
+                            <q-select outlined dense v-model="currentCompany.taxData.fiscalResponsibilities"
+                                :options="fiscalResponsibilities" />
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-4 q-mb-lg">
+                            <div class="q-pb-xs text-subtitle text-weight-medium">Tributos</div>
+                            <q-select outlined dense v-model="currentCompany.taxData.taxes" :options="[]" />
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-4 row items-center justify-around">
+                            <span class="q-pb-xs text-subtitle text-weight-medium">Maneja AIU</span>
+                            <q-checkbox outlined dense type="text" v-model="currentCompany.taxData.hasAIU" />
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-4 row items-center justify-around q-mb-md">
+                            <div class="q-pb-xs text-subtitle text-weight-medium row items-center justify-around">
+                                Utilizo dos impuestos cargos en la factura de venta
+                            </div>
+                            <q-checkbox outlined dense type="text" v-model="currentCompany.taxData.hasDoubleTax" />
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-4 row items-center justify-around q-mb-md">
+                            <div class="q-pb-xs text-subtitle text-weight-medium">
+                                Es agente retenedor del impuesto sobre las ventas IVA
+                            </div>
+                            <q-checkbox outlined dense type="text"
+                                v-model="currentCompany.taxData.isWithholdingAgent" />
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-4 row items-center justify-around q-mb-md">
+                            <div class="q-pb-xs text-subtitle text-weight-medium">
+                                Maneja impuesto Ad-Valorem (para industrias de licores)
+                            </div>
+                            <q-checkbox outlined dense type="text" v-model="currentCompany.taxData.hasAdValoremTax" />
                         </div>
                     </div>
 
-                    <div v-else-if="selectedTab === 2">
+                    <div v-else-if="selectedTab === 2" class="row  q-col-gutter-x-xl">
                         <div class="col-12 text-h6 text-bold">
-                            Representante
+                            Representante Legal
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-4">
+                            <div class="q-pb-xs text-subtitle text-weight-medium">Nombres</div>
+                            <q-input outlined dense type="text" v-model="currentCompany.legalRepresentative.names"
+                                :rules="[(val: string) => (val && val.length > 0) || 'Debes completar este campo']" />
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-4">
+                            <div class="q-pb-xs text-subtitle text-weight-medium">Apellidos</div>
+                            <q-input outlined dense type="text" v-model="currentCompany.legalRepresentative.lastNames"
+                                :rules="[(val: string) => (val && val.length > 0) || 'Debes completar este campo']" />
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-4">
+                            <div class="q-pb-xs text-subtitle text-weight-medium">Tipo Identificación</div>
+                            <q-select outlined dense v-model="currentCompany.legalRepresentative.documentType"
+                                :options="documentTypes"
+                                :rules="[(val: string) => (val && val.length > 0) || 'Debes completar este campo']">
+                            </q-select>
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-4">
+                            <div class="q-pb-xs text-subtitle text-weight-medium">Identificación</div>
+                            <q-input outlined dense type="number"
+                                v-model.number="currentCompany.legalRepresentative.documentNumber"
+                                :rules="[(val: number) => (val && val > 0) || 'Debes completar este campo']" />
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-4">
+                            <div class="q-pb-xs text-subtitle text-weight-medium">¿Tienes socios en la empresa?</div>
+                            <div class="row justify-around">
+                                <q-radio v-model="currentCompany.legalRepresentative.hasPartners" label="Si"
+                                    :val="true" />
+                                <q-radio v-model="currentCompany.legalRepresentative.hasPartners" label="No"
+                                    :val="false" />
+                            </div>
                         </div>
                     </div>
 
@@ -113,31 +219,6 @@
                         </div>
                     </div>
 
-                    <!-- <div class="col-12 col-sm-6 col-md-4">
-                        <div class="text-subtitle text-weight-medium">Nombres</div>
-                        <q-input outlined dense type="text" v-model="currentCompany.names"
-                            :rules="[(val: string) => (val && val.length > 0) || 'Debes completar este campo']" />
-                    </div>
-                    <div class="col-12 col-sm-6 col-md-4">
-                        <div class="text-subtitle text-weight-medium">Apellidos</div>
-                        <q-input outlined dense type="text" v-model="currentCompany.lastNames"
-                            :rules="[(val: string) => (val && val.length > 0) || 'Debes completar este campo']" />
-                    </div>
-                    <div class="col-12 col-sm-6 col-md-4">
-                        <div class="text-subtitle text-weight-medium">Tipo Documento</div>
-                        <q-input outlined dense type="text" v-model="currentCompany.documentType"
-                            :rules="[(val: string) => (val && val.length > 0) || 'Debes completar este campo']" />
-                    </div>
-                    <div class="col-12 col-sm-6 col-md-4">
-                        <div class="text-subtitle text-weight-medium">Numero Documento</div>
-                        <q-input outlined dense type="number" v-model.number="currentCompany.documentNumber"
-                            :rules="[(val: number) => (val && val > 0) || 'Debes completar este campo']" />
-                    </div>
-                    <div class="col-12 col-sm-6 col-md-4">
-                        <div class="text-subtitle text-weight-medium">Correo Electrónico</div>
-                        <q-input outlined dense type="text" v-model="currentCompany.email"
-                            :rules="[(val: string) => (val && val.length > 0) || 'Debes completar este campo']" />
-                    </div> -->
                 </q-form>
             </q-card-section>
 
@@ -161,6 +242,53 @@ const props = defineProps({
 const isShowingDialog = ref<boolean>(false);
 const currentCompany = ref(new CompanyModel({}));
 const selectedTab = ref(0);
+const documentTypes = [
+    'Registro civil',
+    'Tarjeta de identidad',
+    'Cédula de ciudadanía',
+    'Tarjeta de extranjería',
+    'tarjeta extranjera',
+    'NIT',
+    'Pasaporte',
+    'Documento de identificación extranjero',
+    'NUIP',
+    'No obligado a registrarse en el RUT PN',
+    'Permiso especial de permanencia PEP',
+    'Sin identificación del exterior o para uso definido por la DIAN',
+    'Nit de otro pais/Sin identificación del exterior (43 medios magnéticos)',
+    'Salvoconducto de permanencia'
+];
+const cities = [
+    'Bucaramanga (002)',
+    'Floridablanca (052)',
+    'Piedecuesta (285)',
+    'Giron (351)',
+];
+const regimeTypeOptions = [
+    '001 Gran Contribuyente',
+    '002 Responsable de IVA',
+    '003 No responsable de IVA',
+    '004 Empresa del Estado',
+];
+const economicActivities = [
+    '0010 Asalariado',
+    '0020 Pensionados',
+    '0081 Personas naturales y sucesiones ilíquidas sin actividad económica',
+    '0082 Personas naturales subsidiadas por terceros',
+    '0090 Rentistas de Capital, solo para personas naturales',
+    '0111 Cultivo de cereales (excepto arroz), legumbres y semillas oleaginosas',
+    '0112 Cultivo de arroz',
+    '0113 Cultivo de hortalizas, raíces y tuberculos',
+    '0114 Cultivo de tabaco',
+    '0115 Cultivo de plantas textiles',
+];
+const fiscalResponsibilities = [
+    'Gran contribuyente',
+    'Autorretenedor',
+    'Agente de retención IVA',
+    'Régimen simple de tributación',
+    'No aplica - Otros',
+];
 
 const tabsMenu = [
     { name: 'General', value: 0 },
